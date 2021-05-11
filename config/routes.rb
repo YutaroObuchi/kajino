44a332a16a1231b75107a66cd65ac617e752e7f1
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-12 00:31:31 +0900

    [add]ユーザー登録・ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get    '/login', to: 'user_sessions#new'
+  post   '/login', to: 'user_sessions#create'
+  delete '/logout', to: 'user_sessions#destroy'
+
   resources :groups, only: [:new, :create, :show] do
     #collection do
       #get 'percentage'
