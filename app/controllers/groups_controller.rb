ace46f7213400d614301b91c771808a068a49f29
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-14 00:49:06 +0900

    [modify]グループ作成後、マイページに遷移するようパスの変更

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,7 @@ class GroupsController < ApplicationController
     @user = current_user
     @group = @user.groups.build(group_params)
      if @user.save
-       redirect_to new_group_path, success: t('.success')
+       redirect_to group_path(@group), success: t('.success')
      else
        flash.now[:danger] = t('.fail')
        render :new
