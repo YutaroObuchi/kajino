925f7465bfbad792f1d201a74937ab595eb74099
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-13 23:00:04 +0900

    [add]アソシエーションの定義

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
+
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: true }
+
+  has_many :users, through: :group_users
+  has_many :group_users
 end
