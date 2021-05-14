8baaa40ccaa5182c008b732ea30db3650a701dd2
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-15 01:00:31 +0900

    kajiテーブルにgroup_idが挿入されるよう実装

--- app/controllers/kajis_controller.rb
+++ app/controllers/kajis_controller.rb
@@ -1,7 +1,24 @@
 class KajisController < ApplicationController
   def new
+  	@kaji = Kaji.new
   end
 
   def create
+    search_record = GroupUser.find_by(user_id: current_user.id)
+    group = Group.find(search_record.group_id)
+    @kaji = group.kajis.build(kaji_params)
+
+    if @kaji.save
+      redirect_to root_path, success: t('.success')
+    else
+      flash.now[:danger] = t('.fail')
+      render :new
+    end
+  end
+
+  private
+
+  def kaji_params
+  	params.require(:kaji).permit(:name, :score)
   end
 end
--- app/models/kaji.rb
+++ app/models/kaji.rb
@@ -1,5 +1,5 @@
 class Kaji < ApplicationRecord
-　validates :name, presence: true
+  validates :name, presence: true
   validates :score, presence: true
   belongs_to :group
 end
