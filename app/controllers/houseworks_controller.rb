75dda8017debc6a23fb29176221acb081ad193b8
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-16 22:11:29 +0900

    [add]houseworkの一括登録機能

--- app/controllers/houseworks_controller.rb
+++ app/controllers/houseworks_controller.rb
@@ -1,24 +1,19 @@
 class HouseworksController < ApplicationController
   def new
   	@housework = Housework.new
+  	search_record = GroupUser.find_by(user_id: current_user.id)
+  	@group = Group.find(search_record.group_id)
+  	@time = Time.current
   end
 
   def create
-    search_record = GroupUser.find_by(user_id: current_user.id)
-    group = Group.find(search_record.group_id)
-    @housework = group.houseworks.build(housework_params)
-
-    if @housework.save
-      redirect_to root_path, success: t('.success')
-    else
-      flash.now[:danger] = t('.fail')
-      render :new
-    end
+    array =  params[:housework][:houseworks]
+    Housework.insert_all(array)
   end
 
   private
 
   def housework_params
-  	params.require(:housework).permit(:name, :score)
+  	params.require(:housework).permit(houseworks: [%w(name score created_at updated_at)])
   end
 end
