a054ffca81e8f7b3e02fc421a7cec176319dee5a
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-25 22:03:59 +0900

    [add]テストファイルのみ作成

---
+++ spec/controllers/calculations_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+RSpec.describe 'houseworkポイントの計算', type: :system do
+
+  let(:group) {create :group}
+  let(:user) {create :user}
+  let(:partner) {create :partner}
+
+  before do
+    login(user)
+    creategroup
+    create_houseworks(group)
+  end
+
+  describe '日次の家事の入力について' do
+    before do
+      login(partner)
+      visit new_calculation_path
+    end
+    fit '入力が正常に動作する' do
+      fill_in 'お風呂掃除', with: 1
+      fill_in '食器洗い', with: 3
+      fill_in '洗濯', with: 0
+      fill_in '掃除機かけ', with: 1
+      fill_in '料理', with: 3
+      click_button '入力する'
+      expect(page).to have_content '今日の家事が入力されました'
+    end
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,12 @@ FactoryBot.define do
     sequence(:email) { |n| "zzz@gamil.com" }
   	password { 'password' }
   	password_confirmation { 'password' }
+
+    trait :partner do
+      sequence(:name) { |n| "user#{n}" }
+      sequence(:email) { |n| "partner@gamil.com" }
+  	  password { 'password1' }
+  	  password_confirmation { 'password' }
+    end
   end
 end
