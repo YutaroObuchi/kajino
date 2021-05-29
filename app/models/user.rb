adcfc6ff3868b2043a5f9d793b55948e9ee43360
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-29 23:04:08 +0900

    [add]UseモデルとGroupモデルにアソシエーション定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
 
   has_many :group_users
   has_many :groups, through: :group_users
+
+  has_many :invitations, class_name: "Invite", foreign_key: 'recipient_id'
+  has_many :sent_invites, class_name: "Invite", foreign_key: 'sender_id'
 end
