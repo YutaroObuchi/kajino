8cae10d4364435ec3a998afe7071b152db6d9276
YutaroObuchi <buchiyu-10.one@docomo.ne.jp>
2021-05-29 23:50:03 +0900

    [add]createアクションに定義

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -3,4 +3,20 @@ class InvitesController < ApplicationController
   	insert_setup
   	@invite = Invite.new
   end
+
+  def create
+    @invite = Invite.new(invite_params)
+    @invite.sender_id = current_user.id
+    if @invite.save
+      InviteMailer.new_user_invite(@invite, new_user_path(invite_token: @invite.token)).deliver
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def invite_params
+    params.require(:invite).permit(:email)
+  end
 end
